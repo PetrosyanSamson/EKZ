1e3b85a0828941cf294d776c6762eec38a5d871a
SmirnovAlexey88 <144774166+SmirnovAlexey88@users.noreply.github.com>
2024-01-11 21:12:01 +0300

    Add files via upload

--- README.js
+++ README.js
@@ -49,3 +49,60 @@ $('#slick-views-our-clients-block-1-2-slider').slick({
         }
     }]
 });
+$('#slick-views-our-clients-attachment-1-3-slider').slick({
+    mobileFirst:false,
+    autoplay:true,
+    infinite: true,
+    pauseOnHover:false,
+    arrows:false,
+    slidesToShow:5,
+    centerMode: true,
+    centerPadding: '10%',
+    responsive:[{
+        breakpoint:1024,
+        settings:{
+            autoplay:true,
+            pauseOnHover:false,
+            arrows:false,
+            slidesToShow:3,
+            waitForAnimate:false}},
+        {
+        breakpoint:600,
+        settings:{
+            autoplay:true,
+            pauseOnHover:false,
+            arrows:false,
+            slidesToShow:2,
+            waitForAnimate:false
+        }
+        }]
+});
+
+$(document).ready(function() {
+    $('.menu.nav.navbar-nav > li').click(function() {
+        $(this).find('.dropdown-menu-wrapper').slideToggle('fast');
+    });
+});
+
+
+
+window.onload = function () {
+    setTimeout(function () {
+        document.querySelector('.loader').style.display = 'none';
+        document.querySelector('header').classList.add('show');
+    }, 2980);
+};
+
+
+
+
+
+// Ждем загрузки DOM. Элемент <body> будет загружен автоматически.
+window.addEventListener('DOMContentLoaded', function() {
+    var button = document.getElementById('button');
+    button.addEventListener('click', function(event) {
+      // Переключаем класс .opened
+      event.target.classList.toggle('opened');
+    });
+  
+  });
